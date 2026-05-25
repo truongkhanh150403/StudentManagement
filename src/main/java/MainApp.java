76475c2f6f5e050be42a361dccda1ef682eb2c0e
Truong Khanh <truongkhanh150403@gmail.com>
2026-05-25 14:40:56 +0800

    Complete Buổi 6: Refactor structure to standard MVC and DAO pattern

--- src/main/java/MainApp.java
+++
@@ -1,9 +0,0 @@
-import com.university.model.Student;
-
-public class MainApp {
-    public static void main(String[] args) {
-        Student std1 = new Student("S01", "Nguyễn Văn A", 20);
-        System.out.println(std1);
-
-    }
-}
